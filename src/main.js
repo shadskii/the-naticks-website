5135255db9a58a1494d28a6d4eae8906b32beb59
Jake Hassel <jake.k.hassel@gmail.com>
2018-10-17 18:48:48 -0400

    Init from cli

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
+import '@babel/polyfill'
 import Vue from 'vue'
+import './plugins/vuetify'
 import App from './App.vue'
+import 'roboto-fontface/css/roboto/roboto-fontface.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import './registerServiceWorker'
 
 Vue.config.productionTip = false
 
