d33730fdd703532a94f1e819ae5f228092dffbc4
Jake Hassel <jake.k.hassel@gmail.com>
2018-10-17 18:55:37 -0400

    Different theme

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,11 +1,12 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
+import colors from 'vuetify/es5/util/colors';
 
 Vue.use(Vuetify, {
   theme: {
-    primary: '#ee44aa',
-    secondary: '#424242',
+    primary: colors.deepOrange.darken1,
+    secondary: colors.deepOrange.darken4,
     accent: '#82B1FF',
     error: '#FF5252',
     info: '#2196F3',
