8b43d275e0897ea408796a4690f6452b01287561
Jake Hassel <jake.k.hassel@gmail.com>
2018-10-20 18:51:01 -0400

    Adding upcoming events

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,69 +1,80 @@
 <template>
   <v-container fluid>
-    <v-slide-y-transition mode="out-in">
-      <v-layout
-        column
-        align-center>
-        <v-avatar
-          size="25em">
-          <img
-            :src="logo"
-            class="img-circle elevation-7 mb-1">
-        </v-avatar>
-        <v-layout justify-space-between>
-          <v-btn
-            fab
-            icon
-            href="https://www.snapchat.com/add/thenaticks"
-            class="body-2">
-            <v-icon>mdi-snapchat</v-icon>
-          </v-btn>
-          <v-btn
-            fab
-            icon
-            href="https://www.instagram.com/thenaticks/"
-            class="body-2">
-            <v-icon>mdi-instagram</v-icon>
-          </v-btn>
-          <v-btn
-            fab
-            icon
-            href="https://soundcloud.com/the-naticks"
-            class="body-2">
-            <v-icon>mdi-soundcloud</v-icon>
-          </v-btn>
-          <v-btn
-            fab
-            icon
-            href="https://www.facebook.com/TheNaticks/"
-            class="body-2">
-            <v-icon>mdi-facebook</v-icon>
-          </v-btn>
-          <v-btn
-            fab
-            icon
-            href="https://twitter.com/TheNaticks"
-            class="body-2">
-            <v-icon>mdi-twitter</v-icon>
-          </v-btn>
-        </v-layout>
-        <blockquote>
-          &#8220;I’d rather be dead than cool.&#8221;
-          <footer>
-            <small>
-              <em>&mdash;Kurt Cobain</em>
-            </small>
-          </footer>
-        </blockquote>
+    <v-layout
+      column
+      align-center>
+      <v-avatar
+        size="25em">
+        <img
+          :src="logo"
+          class="img-circle elevation-7 mb-1">
+      </v-avatar>
+      <v-layout justify-space-between>
+        <v-btn
+          fab
+          icon
+          href="https://www.snapchat.com/add/thenaticks"
+          class="body-2">
+          <v-icon>mdi-snapchat</v-icon>
+        </v-btn>
+        <v-btn
+          fab
+          icon
+          href="https://www.instagram.com/thenaticks/"
+          class="body-2">
+          <v-icon>mdi-instagram</v-icon>
+        </v-btn>
+        <v-btn
+          fab
+          icon
+          href="https://soundcloud.com/the-naticks"
+          class="body-2">
+          <v-icon>mdi-soundcloud</v-icon>
+        </v-btn>
+        <v-btn
+          fab
+          icon
+          href="https://www.facebook.com/TheNaticks/"
+          class="body-2">
+          <v-icon>mdi-facebook</v-icon>
+        </v-btn>
+        <v-btn
+          fab
+          icon
+          href="https://twitter.com/TheNaticks"
+          class="body-2">
+          <v-icon>mdi-twitter</v-icon>
+        </v-btn>
       </v-layout>
-    </v-slide-y-transition>
+      <blockquote>
+        &#8220;I’d rather be dead than cool.&#8221;
+        <footer>
+          <small>
+            <em>&mdash;Kurt Cobain</em>
+          </small>
+        </footer>
+      </blockquote>
+
+    </v-layout>
+    <v-layout
+      row
+      align-center
+      justify-space-around>
+      <v-flex lg8>
+        <events/>
+      </v-flex>
+    </v-layout>
   </v-container>
 </template>
 
 <script>
 import logo from '../assets/logo.png';
+import Events from './Events.vue';
 export default {
   name: 'HelloWorld',
+  components: {
+    Events,
+  },
   data() {
     return {
       logo: logo,
