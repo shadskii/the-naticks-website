5ff49766bcb916d8ac349d19980221a435e4c95b
Jake Hassel <jake.k.hassel@gmail.com>
2018-11-01 22:24:05 -0400

    Remove empty drawer

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,5 @@
 <template>
   <v-app dark>
-    <v-navigation-drawer
-      v-model="drawer"
-      persistent
-      disable-resize-watcher
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-tile
-          v-for="(item, i) in items"
-          :key="i"
-          value="true"
-        >
-          <v-list-tile-action>
-            <v-icon v-html="item.icon"/>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title v-text="item.title"/>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
     <v-toolbar
       color="primary"
       app
@@ -45,8 +23,6 @@ export default {
   },
   data() {
     return {
-      drawer: false,
-      items: [],
       title: 'The Naticks',
     };
   },
