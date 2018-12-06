65271bd5163c7cc068149528efa18a5152315ba4
Jake Hassel <jake.k.hassel@gmail.com>
2018-12-05 22:15:45 -0500

    Remove old events

--- src/shows.js
+++ src/shows.js
@@ -18,8 +18,6 @@ class Event {
 }
 
 export default [
-  new Event('November 23rd', 'The Pub', 'Matunuck, RI', 'Acoustic set from 8:30PM to 12:30PM'),
-  new Event('November 24th', 'The Guild', 'Pawtucket, RI', 'Acoustic set from 4:30 to 6:30'),
   new Event('December 8th', 'Kai Bar', 'East Greenwwich, RI', 'Acoustic set from 8:00PM to 11:00PM'),
   new Event('January 26th', 'Whalers Brewing Company', 'Wakefield, RI', 'Full band set at 8:00PM'),
 ];
