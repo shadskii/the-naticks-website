febd0c9aaabd60b8bc54db0c1aa2c6c610855c80
Jake Hassel <jake.k.hassel@gmail.com>
2018-12-05 22:10:01 -0500

    Redesign events

--- src/components/Events.vue
+++ src/components/Events.vue
@@ -1,33 +1,28 @@
 <template>
   <div>
     <h1 class="headline font-weight-light">Upcoming Events</h1>
-    <v-card>
-      <!-- <v-card-title>
+    <v-list three-line>
+      <v-list-tile
+        v-for="event in events"
+        :key="event.date"
+        class="mb-2"
+      >
+        <v-list-tile-content>
+          <v-list-tile-title class="font-weight-bold">
+            {{ event.date }}
+          </v-list-tile-title>
 
-    </v-card-title>
-    <v-divider /> -->
-      <v-list>
-        <v-list-tile
-          v-for="event in events"
-          :key="event.date"
-          class="mb-2"
-          two-line
-        >
-          <v-list-tile-content>
-            <v-list-tile-title>
-              {{ event.date }}
-            </v-list-tile-title>
-            <v-list-tile-sub-title class="text--primary">
-              {{ `${event.venue} - ${event.location}` }}
-            </v-list-tile-sub-title>
-            <v-list-tile-sub-title>
-              {{ event.description }}
-            </v-list-tile-sub-title>
-          </v-list-tile-content>
+          <v-list-tile-sub-title class="text--primary">
+            {{ `${event.venue} - ${event.location}` }}
+          </v-list-tile-sub-title>
+          <v-list-tile-sub-title>
+            {{ event.description }}
+          </v-list-tile-sub-title>
 
-        </v-list-tile>
-      </v-list>
-    </v-card>
+        </v-list-tile-content>
+
+      </v-list-tile>
+    </v-list>
   </div>
 </template>
 <script>
