f4ed0604957185af44f7dc0696e5cd5ffb14f793
Jake Hassel <jake.k.hassel@gmail.com>
2018-12-21 23:47:04 -0500

    Starting blog page

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -1,9 +1,28 @@
 <template>
-  <h1>Blog</h1>
+  <v-container>
+    <v-layout
+      row
+      wrap
+      text-xs-center>
+      <v-flex
+        v-for="p in posts"
+        :key="p.title"
+        xs10>
+        <v-card>
+          <v-card-title v-text="p.title" />
+          <v-card-text v-text="p.content" />
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 <script>
 export default {
-
+  data() {
+    return {
+      posts: Array(10).fill(1).map((v, i)=> ({title: `post: ${i}`, content: `content content`})),
+    };
+  },
 };
 </script>
 
