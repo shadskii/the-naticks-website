4bca47e2c4e51ffd2263c15841f773a1ba9c3ecc
Jake Hassel <jake.k.hassel@gmail.com>
2018-12-24 18:59:46 -0500

    Align to center and space posts

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -3,12 +3,14 @@
     <v-layout
       row
       wrap
+      justify-center
       text-xs-center>
       <v-flex
         v-for="p in posts"
         :key="p.title"
+        class="pb-3"
         xs10>
-        <v-card>
+        <v-card >
           <v-card-title v-text="p.title" />
           <v-card-text v-text="p.content" />
         </v-card>
