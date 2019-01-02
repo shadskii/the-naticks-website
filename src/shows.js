bf0bce47aeaf6cbb26b696637a0da4da9bd38175
Jake Hassel <jake.k.hassel@gmail.com>
2019-01-01 22:55:09 -0500

    :pencil: Update shows

--- src/shows.js
+++ src/shows.js
@@ -18,6 +18,12 @@ class Event {
 }
 
 export default [
-  new Event('December 8th', 'Kai Bar', 'East Greenwwich, RI', 'Acoustic set from 8:00PM to 11:00PM'),
+  new Event('January 11th', 'Kai Bar', 'East Greenwwich, RI', 'Acoustic set from 8:00PM to 11:00PM'),
+  new Event('January 16th', 'Trillium Brewery', 'Canton, MA', 'Details TBA'),
+  new Event('January 18th', 'Parlor Bar and Kitchen', 'Newport, RI', 'Details TBA'),
+  new Event('January 25th', 'The Pub', 'Matunuck, RI', 'Acoustic set from 8:00PM to 12:00AM'),
   new Event('January 26th', 'Whalers Brewing Company', 'Wakefield, RI', 'Full band set at 8:00PM'),
+  new Event('January 27th', 'Jamestown Library', 'Jamestown, RI', 'Full band set from 3:00PM to 4:30PM'),
+  new Event('January 31st', 'Pumphouse Mustic Works', 'Wakefield, RI', 'Full band set, details TBA'),
+  new Event('February 8th', 'Parlor Bar and Kitchen', 'Newport, RI', 'Acoustic set, details TBA'),
 ];
